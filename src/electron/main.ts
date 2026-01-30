eacc1bddba71a29f914cef9e4ad2b28c72e56a9d
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-30 11:11:17 +0800

    Create the loading page.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -3,12 +3,22 @@ import path from 'path';
 import { Utils } from './utils.js';
 import 'dotenv/config';
 
-app.on('ready', () => {
-  const mainWindow = new BrowserWindow({ backgroundColor: '#1c1917' });
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    show: false,
+    backgroundColor: '#1c1917',
+  });
 
   if (Utils.isDev()) {
     mainWindow.loadURL(`http://localhost:${process.env.PORT}`);
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), './dist-vue/index.html'));
   }
-});
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.maximize();
+    mainWindow.show();
+  });
+};
+
+app.whenReady().then(createWindow);
