9a7f34b1daa5e6a8b7ab7181e785d0ee12480f1e
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-03-22 01:52:16 +0800

    Remove unnecessary type keyword.

--- src/shared/types/mission.d.ts
+++ src/shared/types/mission.d.ts
@@ -1,5 +1,5 @@
 import type { InferSelectModel } from 'drizzle-orm';
-import type { mission } from '../../electron/db/schema/mission.sql';
+import { mission } from '../../electron/db/schema/mission.sql';
 
 export type Mission = InferSelectModel<typeof mission>;
 
