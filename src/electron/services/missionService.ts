44ce2b1c4302ac33c5ea75faa4429ec7ed63e846
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-03-22 14:27:04 +0800

    Add the mission service.

---
+++ src/electron/services/missionService.ts
@@ -0,0 +1,50 @@
+import { mission } from '../db/schema/mission.sql.js';
+import { desc, eq } from 'drizzle-orm';
+import { Mission, MissionDraft } from '../../shared/types/mission.js';
+import { getDb } from '../db/db.js';
+
+export class MissionService {
+  static async getAll() {
+    try {
+      return await getDb().select().from(mission).orderBy(desc(mission.priority));
+    } catch {
+      return false;
+    }
+  }
+
+  static async create(missionDraft: MissionDraft) {
+    try {
+      // Destructure data
+      const data = { updatedAt: new Date(), createdAt: new Date(), ...missionDraft };
+
+      await getDb().insert(mission).values(data);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  static async update(newMission: Mission) {
+    try {
+      // Only update the editable data
+      const { id, createdAt, ...data } = newMission;
+
+      // Update the date
+      data.updatedAt = new Date();
+
+      await getDb().update(mission).set(data).where(eq(mission.id, id));
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  static async delete(missionId: number) {
+    try {
+      await getDb().delete(mission).where(eq(mission.id, missionId));
+      return true;
+    } catch {
+      return false;
+    }
+  }
+}
