12d1e5c24a38fac51f110b7c0f898326f599c3fa
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-04-20 14:25:16 +0800

    Change progress bar style for a thicker look.

--- src/ui/components/misc/ProgressBar.vue
+++ src/ui/components/misc/ProgressBar.vue
@@ -14,7 +14,7 @@ const highlightLimit = computed(() => Math.floor(props.progress * bars));
   <div class="flex -skew-x-20 gap-x-1">
     <motion.div
       v-for="(_, idx) in bars"
-      class="w-2 h-4 bg-primary duration-100"
+      class="w-2 h-6 bg-primary duration-100"
       :initial="{ opacity: 0, x: -10 }"
       :animate="{ opacity: idx >= highlightLimit ? 0.25 : 1, x: 0 }"
       :transition="{ delay: 0.02 * idx }"
